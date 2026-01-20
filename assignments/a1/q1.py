3cbbe140496db30b57853130444d00c494982231
MichaelC49 <yca541@sfu.ca>
2026-01-20 13:39:29 -0800

    Update q1.py

--- assignments/a1/q1.py
+++ assignments/a1/q1.py
@@ -1,9 +1,15 @@
 # q1.py
 
 #
-# Full Name:
-#  SFU ID #:
-# SFU Email:
+# Full Name:Yishao Chen
+#  SFU ID #:301652300
+# SFU Email:yca541@sfu.ca
 #
 
 # ... put your answer to question 1 here ...
+name = input("What's your name? ")
+quarters = int(input("How many quarters do you have? "))
+total_cents = quarters * 25
+candies = total_cents // 12
+remaining_cents = total_cents % 12
+print(f"{name}, you can buy {candies} candies with {remaining_cents} cents left over.")
